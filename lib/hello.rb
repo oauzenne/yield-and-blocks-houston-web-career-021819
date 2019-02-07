b1aef419e030913a833636334bb5aaaed9642d2e
Olivia Auzenne <oauzenne@gmail.com>
2019-02-07 19:03:20 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,15 +1,17 @@
-def hello_t
-
-end
-
 def hello_t(array)
+  if block_given?
   i = 0
- 
-  while i < array.length
-    yield(array[i])
-    i = i + 1
-  end
- 
-  array
+
+
+ while i < array.length
+  yield (array[i])
+  i = i + 1
+end	end
+
+
+ array
+else
+puts "Hey! No block was given!"
 end
+
 
